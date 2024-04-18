06c8e139f92f6ddbb4d9512996d3fa251f91a0cf
Jeffrey Martinez <martinezjf2@gmail.com>
2024-04-18 14:57:53 -0400

    added hover state on the button links

--- components/Navbar.js
+++ components/Navbar.js
@@ -3,53 +3,53 @@ import React from "react";
 
 export default function Navbar() {
     return (
-      <div className="p-[10px] flex justify-around items-center bg-gray-100 shadow">
-        <div className="w-2/12 pl-7 ">LOGO</div>
+      <div className="relative">
+        <div className="flex justify-center items-center bg-white shadow">
+          <div className="w-2/12 pl-7 flex justify-center items-center cursor-pointer">LOGO</div>
 
-        <div className="w-7/12">
-          <div className="relative">
-            <i className="absolute fa-magnifying-glass top-1 left-1 ml-2"></i>
-            <input
-              className="flex w-[95%] bg-gray-200 pl-[2.0rem] rounded h-8 text-sm text-[12px]"
-              type="text"
-              placeholder="Search for sweet gear"
-            />
+          <div className="w-7/12">
+            <div className="relative">
+              <i className="absolute fa-magnifying-glass top-1 left-1 ml-2"></i>
+              <input
+                className="flex w-[95%] bg-gray-100 pl-[2.0rem] rounded h-8 text-sm text-[12px]"
+                type="text"
+                placeholder="Search for sweet gear"
+              />
+            </div>
+          </div>
+
+          <div className="w-2/12 flex justify-center items-center hover:bg-gray-100 hover:text-blue-500 px-4 py-2 cursor-pointer">
+            <div className="text-blue-500 font-semibold p-0 relative">
+              (800) 222-4700
+              <span className="text-[10px] flex justify-items-start">
+                Talk to an expert!
+              </span>
+            </div>
+          </div>
+
+          <div className="w-2/12 flex justify-center items-center hover:bg-gray-100 hover:text-blue-500 px-4 py-3 cursor-pointer">
+            <div className="font-semibold text-[13px]">
+              Contact Us
+              <span className="text-[10px] flex justify-items-start items-center">
+                We're here to help
+              </span>
+            </div>
+          </div>
+
+          <div className="w-2/12 flex justify-center items-center hover:bg-gray-100 hover:text-blue-500 px-4 py-5 cursor-pointer">
+            <div className="font-semibold text-[13px]">
+              Account
+              {/* <span className="text-[10px] flex">Arrow Down</span> */}
+            </div>
+          </div>
+
+          <div className="w-2/12 flex justify-center items-center hover:bg-gray-100 hover:text-blue-500 px-4 py-5 cursor-pointer">
+            <div className="font-semibold text-[13px]">
+              Account
+              {/* <span className="text-[10px] flex">Arrow Down</span> */}
+            </div>
           </div>
         </div>
-
-        <div className="w-2/12">
-          <div className="text-blue-500 font-semibold p-0 relative">
-            (800) 222-4700
-            <span className="text-[10px] flex justify-items-start">
-              Talk to an expert!
-            </span>
-          </div>
-        </div>
-
-        <div className="w-2/12 relative">
-          <div className="font-semibold text-[13px]">
-            Contact Us
-            <span className="text-[10px] flex justify-items-start items-center">
-              We're here to help
-            </span>
-          </div>
-        </div>
-
-        <div className="w-2/12 relative">
-          <div className="font-semibold text-[13px]">
-            Account
-            {/* <span className="text-[10px] flex">Arrow Down</span> */}
-          </div>
-        </div>
-
-        <div className="w-2/12 relative">
-          <div className="font-semibold text-[13px]">
-            Account
-            {/* <span className="text-[10px] flex">Arrow Down</span> */}
-          </div>
-        </div>
-
-        
       </div>
     );
 }
--- components/SecondNavbar.js
+++ components/SecondNavbar.js
@@ -3,28 +3,28 @@ import React from 'react';
 export default function SecondNavbar() {
     return (
       <div className="flex justify-center shadow ">
-        <div className="text-[12px] font-medium py-2 px-9 hover:bg-gray-200 hover:text-blue-500">
+        <div className="text-[12px] font-medium py-2 px-9 hover:bg-gray-100 hover:text-blue-500 cursor-pointer">
           Shop By Category
         </div>
-        <div className="text-[12px] font-medium py-2 px-9 hover:bg-gray-200 hover:text-blue-500">
+        <div className="text-[12px] font-medium py-2 px-9 hover:bg-gray-100 hover:text-blue-500 cursor-pointer">
           What's New
         </div>
-        <div className="text-[12px] font-medium py-2 px-9 hover:bg-gray-200 hover:text-blue-500">
+        <div className="text-[12px] font-medium py-2 px-9 hover:bg-gray-100 hover:text-blue-500 cursor-pointer">
           Deals
         </div>
-        <div className="text-[12px] font-medium py-2 px-9 hover:bg-gray-200 hover:text-blue-500">
+        <div className="text-[12px] font-medium py-2 px-9 hover:bg-gray-100 hover:text-blue-500 cursor-pointer">
           Used Gear
         </div>
-        <div className="text-[12px] font-medium py-2 px-9 hover:bg-gray-200 hover:text-blue-500">
+        <div className="text-[12px] font-medium py-2 px-9 hover:bg-gray-100 hover:text-blue-500 cursor-pointer">
           Rentals
         </div>
-        <div className="text-[12px] font-medium py-2 px-9 hover:bg-gray-200 hover:text-blue-500">
+        <div className="text-[12px] font-medium py-2 px-9 hover:bg-gray-100 hover:text-blue-500 cursor-pointer">
           Articles & Videos
         </div>
-        <div className="text-[12px] font-medium py-2 px-9 hover:bg-gray-200 hover:text-blue-500">
+        <div className="text-[12px] font-medium py-2 px-9 hover:bg-gray-100 hover:text-blue-500 cursor-pointer">
           Giveaway
         </div>
-        <div className="text-[12px] font-medium py-2 px-9 hover:bg-gray-200 hover:text-blue-500">
+        <div className="text-[12px] font-medium py-2 px-9 hover:bg-gray-100 hover:text-blue-500 cursor-pointer">
           Music Store Card
         </div>
       </div>
