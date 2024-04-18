095c3cb57b28c47ac44ffaceb3e39c79a8e13ab1
Jeffrey Martinez <martinezjf2@gmail.com>
2024-04-18 14:33:59 -0400

    finished the second navbar

--- components/SecondNavbar.js
+++ components/SecondNavbar.js
@@ -2,15 +2,31 @@ import React from 'react';
 
 export default function SecondNavbar() {
     return (
-      <div className='flex justify-evenly items-center py-1 shadow '>
-        <div>Shop By Category</div>
-        <div>What's New</div>
-        <div>Deals</div>
-        <div>Used Gear</div>
-        <div>Rentals</div>
-        <div>Articles & Videos</div>
-        <div>Giveaway</div>
-        <div>Music Store Card</div>
+      <div className="flex justify-center shadow ">
+        <div className="text-[12px] font-medium py-2 px-9 hover:bg-gray-200 hover:text-blue-500">
+          Shop By Category
+        </div>
+        <div className="text-[12px] font-medium py-2 px-9 hover:bg-gray-200 hover:text-blue-500">
+          What's New
+        </div>
+        <div className="text-[12px] font-medium py-2 px-9 hover:bg-gray-200 hover:text-blue-500">
+          Deals
+        </div>
+        <div className="text-[12px] font-medium py-2 px-9 hover:bg-gray-200 hover:text-blue-500">
+          Used Gear
+        </div>
+        <div className="text-[12px] font-medium py-2 px-9 hover:bg-gray-200 hover:text-blue-500">
+          Rentals
+        </div>
+        <div className="text-[12px] font-medium py-2 px-9 hover:bg-gray-200 hover:text-blue-500">
+          Articles & Videos
+        </div>
+        <div className="text-[12px] font-medium py-2 px-9 hover:bg-gray-200 hover:text-blue-500">
+          Giveaway
+        </div>
+        <div className="text-[12px] font-medium py-2 px-9 hover:bg-gray-200 hover:text-blue-500">
+          Music Store Card
+        </div>
       </div>
     );
 }
