fa3094905fe64b29ba55e7a26ae3bfa77c623410
Jeffrey Martinez <martinezjf2@gmail.com>
2024-04-18 19:57:38 -0400

    adjusted teh padding on the first header div

--- components/Header.js
+++ components/Header.js
@@ -3,12 +3,12 @@ import React from "react";
 export default function Header() {
   return (
     <div className="flex w-[80%] mx-auto my-4">
-      <div className="bg-black text-white h-[18rem] w-8/12 py-11 px-5 relative container">
+      <div className="bg-black text-white h-[18rem] w-8/12 py-11 px-9 relative container">
         <div className="font-bold text-[48px] mb-1">Electric Guitar Month</div>
         <div className="text-[18px] font-light">
           Deals, Special Financing, New Gear & More
         </div>
-        <div className="absolute bottom-7">
+        <div className="absolute bottom-9">
           <button className="bg-red-600 py-1.5 px-8 rounded font-semibold text-[13px]">
             Shop Now
           </button>
@@ -16,7 +16,7 @@ export default function Header() {
         </div>
       </div>
 
-      <div className="bg-white text-blue-700 w-4/12 text-center  border-blue-500 border-2 ml-3 relative h-[18rem]">
+      <div className="bg-white text-[#0273ba] w-4/12 text-center  border-[#0273ba] border-2 ml-3 relative h-[18rem]">
         <div className="py-3 px-4">
           <div className="text-[70px] font-extrabold leading-none">
             0<span className="text-[30px]">%</span>
@@ -30,9 +30,11 @@ export default function Header() {
             Now-April 30.
           </div>
         </div>
-        <div className="bg-blue-400 h-[5.2rem] leading-none">
+        <div className="bg-[#0273ba] h-[5.2rem] leading-none">
           <div className="">
-            <button className="mt-4 bg-white py-2 px-8 text-[13px] rounded font-semibold">Shop Now</button>
+            <button className="mt-4 bg-white py-2 px-8 text-[13px] rounded font-semibold">
+              Shop Now
+            </button>
           </div>
         </div>
       </div>
