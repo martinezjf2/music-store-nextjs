b018e83bf43f2ccd88acfc53fdcc9cfff1bb161f
Jeffrey Martinez <martinezjf2@gmail.com>
2024-04-18 20:49:59 -0400

    added the flex class to the second navbar links

--- components/SecondNavbar.js
+++ components/SecondNavbar.js
@@ -7,31 +7,31 @@ import {
 export default function SecondNavbar() {
     return (
       <div className="flex justify-center shadow ">
-        <div className="text-[12px] font-medium py-2 px-9 hover:bg-gray-100 hover:text-blue-500 cursor-pointer">
+        <div className="text-[12px] font-medium py-2 px-9 hover:bg-gray-100 hover:text-blue-500 cursor-pointer flex">
           Shop By Category
           <FontAwesomeIcon icon={faChevronDown} className=" ml-2" />
         </div>
-        <div className="text-[12px] font-medium py-2 px-9 hover:bg-gray-100 hover:text-blue-500 cursor-pointer">
+        <div className="text-[12px] font-medium py-2 px-9 hover:bg-gray-100 hover:text-blue-500 cursor-pointer flex">
           What's New
         </div>
-        <div className="text-[12px] font-medium py-2 px-9 hover:bg-gray-100 hover:text-blue-500 cursor-pointer">
+        <div className="text-[12px] font-medium py-2 px-9 hover:bg-gray-100 hover:text-blue-500 cursor-pointer flex">
           Deals
           <FontAwesomeIcon icon={faChevronDown} className=" ml-2" />
         </div>
-        <div className="text-[12px] font-medium py-2 px-9 hover:bg-gray-100 hover:text-blue-500 cursor-pointer">
+        <div className="text-[12px] font-medium py-2 px-9 hover:bg-gray-100 hover:text-blue-500 cursor-pointer flex">
           Used Gear
           <FontAwesomeIcon icon={faChevronDown} className=" ml-2" />
         </div>
-        <div className="text-[12px] font-medium py-2 px-9 hover:bg-gray-100 hover:text-blue-500 cursor-pointer">
+        <div className="text-[12px] font-medium py-2 px-9 hover:bg-gray-100 hover:text-blue-500 cursor-pointer flex">
           Rentals
         </div>
-        <div className="text-[12px] font-medium py-2 px-9 hover:bg-gray-100 hover:text-blue-500 cursor-pointer">
+        <div className="text-[12px] font-medium py-2 px-9 hover:bg-gray-100 hover:text-blue-500 cursor-pointer flex">
           Articles & Videos
         </div>
-        <div className="text-[12px] font-medium py-2 px-9 hover:bg-gray-100 hover:text-blue-500 cursor-pointer">
+        <div className="text-[12px] font-medium py-2 px-9 hover:bg-gray-100 hover:text-blue-500 cursor-pointer flex">
           Giveaway
         </div>
-        <div className="text-[12px] font-medium py-2 px-9 hover:bg-gray-100 hover:text-blue-500 cursor-pointer">
+        <div className="text-[12px] font-medium py-2 px-9 hover:bg-gray-100 hover:text-blue-500 cursor-pointer flex">
           Music Store Card
         </div>
       </div>
