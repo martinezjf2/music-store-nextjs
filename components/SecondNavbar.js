6c14b2fb66e4239a6f4124fc2629b696ae539f52
Jeffrey Martinez <martinezjf2@gmail.com>
2024-04-18 20:03:27 -0400

    added chevron icon to the second navbar

--- components/SecondNavbar.js
+++ components/SecondNavbar.js
@@ -1,19 +1,23 @@
 import React from 'react';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faChevronDown
+} from "@fortawesome/free-solid-svg-icons";
 
 export default function SecondNavbar() {
     return (
       <div className="flex justify-center shadow ">
         <div className="text-[12px] font-medium py-2 px-9 hover:bg-gray-100 hover:text-blue-500 cursor-pointer">
-          Shop By Category
+          Shop By Category <FontAwesomeIcon icon={faChevronDown} className=" ml-1" />
         </div>
         <div className="text-[12px] font-medium py-2 px-9 hover:bg-gray-100 hover:text-blue-500 cursor-pointer">
           What's New
         </div>
         <div className="text-[12px] font-medium py-2 px-9 hover:bg-gray-100 hover:text-blue-500 cursor-pointer">
-          Deals
+          Deals <FontAwesomeIcon icon={faChevronDown} className=" ml-1" />
         </div>
         <div className="text-[12px] font-medium py-2 px-9 hover:bg-gray-100 hover:text-blue-500 cursor-pointer">
-          Used Gear
+          Used Gear <FontAwesomeIcon icon={faChevronDown} className=" ml-1" />
         </div>
         <div className="text-[12px] font-medium py-2 px-9 hover:bg-gray-100 hover:text-blue-500 cursor-pointer">
           Rentals
