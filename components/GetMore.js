3394d3b12707584902db8610026f334588f93d15
Jeffrey Martinez <martinezjf2@gmail.com>
2024-04-20 13:24:10 -0400

    fixed the get more learn more link

--- components/GetMore.js
+++ components/GetMore.js
@@ -7,23 +7,22 @@ export default function GetMore() {
           Get More at GuitarStore
         </div>
         <div className="flex  md:w-[80%] m-auto pt-10">
-          
-        {/* First Div */}
-          <div className="border rounded border-gray-200 pt-3 px-4 pb-8 w-full mx-1 relative cursor-pointer hover:border-blue-700 hover:shadow-lg hover:scale-105 hover:duration-200">
-            <div className="w-[55px] h-[55px] border-gray-200 border bg-white rounded-full absolute -top-8 right-[3.7rem] hover:border-blue-700 "></div>
-            <div className="text-[12px] text-[#0273ba] pt-5 px-2 pb-1 font-semibold">
+          {/* First Div */}
+          <div className="border rounded border-gray-200 pt-3 px-4 pb-3 w-full mx-1 relative cursor-pointer hover:border-blue-700 hover:shadow-lg hover:scale-105 hover:duration-200">
+            <div className="text-[12px] text-[#0273ba] pt-5 px-2 pb-1 font-semibold relative">
+              <div className="w-[55px] h-[55px] border-gray-200 border bg-white rounded-full absolute -top-[2.9rem] left-[2.7rem] "></div>
               Dedicated Sales Engineer
             </div>
-            <div className="text-[9px] px-1">
+            <div className="text-[9px] px-1 relative">
               Helping you buy music hear with confidence.
-            </div>
-            <div className="text-[#0273ba] text-[10px] font-bold absolute bottom-1 left-[3.6rem] text-center pb-1">
-              Learn More
+              <div className="text-[#0273ba] text-[10px] font-bold mt-6">
+                Learn More
+              </div>
             </div>
           </div>
-         
-        {/* Second Div */}
-          <div className="border rounded border-gray-200 pt-3 px-4 pb-8 w-full mx-1 relative cursor-pointer hover:border-blue-700 hover:shadow-lg hover:scale-105 hover:duration-200">
+
+          {/* Second Div Starting Thought */}
+          {/* <div className="border rounded border-gray-200 pt-3 px-4 pb-8 w-full mx-1 relative cursor-pointer hover:border-blue-700 hover:shadow-lg hover:scale-105 hover:duration-200">
             <div className="w-[55px] h-[55px] border-gray-200 border bg-white rounded-full absolute -top-8 right-[3.7rem] hover:border-blue-700 "></div>
             <div className="text-[12px] text-[#0273ba] pt-5 px-2 pb-1 font-semibold">
               55-point Guitar Inpsection
@@ -34,59 +33,76 @@ export default function GetMore() {
             <div className="text-[#0273ba] text-[10px] font-bold absolute bottom-1 left-[3.6rem] text-center pb-1">
               Learn More
             </div>
-          </div>
-          
-        {/* Third Div */}
-          <div className="border rounded border-gray-200 pt-3 px-4 pb-8 w-full mx-1 relative cursor-pointer hover:border-blue-700 hover:shadow-lg hover:scale-105 hover:duration-200">
-            <div className="w-[55px] h-[55px] border-gray-200 border bg-white rounded-full absolute -top-8 right-[3.7rem] hover:border-blue-700 "></div>
-            <div className="text-[12px] text-[#0273ba] pt-5 px-2 pb-1 font-semibold">
-              Fast, FREE Shipping
+          </div> */}
+          <div className="border rounded border-gray-200 pt-3 px-4 pb-3 w-full mx-1 relative cursor-pointer hover:border-blue-700 hover:shadow-lg hover:scale-105 hover:duration-200">
+            <div className="text-[12px] text-[#0273ba] pt-5 px-2 pb-1 font-semibold relative">
+              <div className="w-[55px] h-[55px] border-gray-200 border bg-white rounded-full absolute -top-[2.9rem] left-[2.7rem] "></div>
+              Dedicated Sales Engineer
             </div>
-            <div className="text-[9px] px-1">Even on the small stuff.</div>
-            <div className="text-[#0273ba] text-[10px] font-bold absolute bottom-1 left-[3.6rem] text-center pb-1">
-              Learn More
-            </div>
-          </div>
-          
-        {/* Fourth Div */}
-          <div className="border rounded border-gray-200 pt-3 px-4 pb-8 w-full mx-1 relative cursor-pointer hover:border-blue-700 hover:shadow-lg hover:scale-105 hover:duration-200">
-            <div className="w-[55px] h-[55px] border-gray-200 border bg-white rounded-full absolute -top-8 right-[3.7rem] hover:border-blue-700 "></div>
-            <div className="text-[12px] text-[#0273ba] pt-5 px-2 pb-1 font-semibold">
-              FREE GuitarStore Support
-            </div>
-            <div className="text-[9px] px-1">
-              Got a question? We're here to help.
-            </div>
-            <div className="text-[#0273ba] text-[10px] font-bold absolute bottom-1 left-[3.6rem] text-center pb-1">
-              Learn More
+            <div className="text-[9px] px-1 relative">
+              Helping you buy music hear with confidence.
+              <div className="text-[#0273ba] text-[10px] font-bold mt-6">
+                Learn More
+              </div>
             </div>
           </div>
-          
-        {/* Fifth Div */}
-          <div className="border rounded border-gray-200 w-full pt-3 px-4 pb-8 mx-1 relative cursor-pointer hover:border-blue-700 hover:shadow-lg hover:scale-105 hover:duration-200">
-            <div className="w-[55px] h-[55px] border-gray-200 border bg-white rounded-full absolute -top-8 right-[3.7rem] hover:border-blue-700 "></div>
-            <div className="text-[12px] text-[#0273ba] pt-5 px-2 pb-1 font-semibold">
-              FREE 2-year Warranty
-            </div>
-            <div className="text-[9px] px-1">Buy with confidence.</div>
-            <div className="text-[#0273ba] text-[10px] font-bold absolute bottom-1 left-[3.6rem] text-center pb-1">
-              Learn More
-            </div>
-          </div>
-          
-        {/* Sixth Div */}
-          <div className="border rounded border-gray-200 pt-3 px-4 pb-8 w-full mx-1 relative cursor-pointer hover:border-blue-700 hover:shadow-lg hover:scale-105 hover:duration-200">
-            <div className="w-[55px] h-[55px] border-gray-200 border bg-white rounded-full absolute -top-8 right-[3.7rem] hover:border-blue-700 "></div>
-            <div className="text-[12px] text-[#0273ba] pt-5 px-2 pb-1 font-semibold">
-              Easy Payments
-            </div>
-            <div className="text-[#0273ba] text-[10px] font-bold absolute bottom-1 left-[3.6rem] text-center pb-1">
-              Learn More
-            </div>
 
-            <div className="text-[9px] px-1">Up to 48 Months.</div>
+          {/* Third Div */}
+          <div className="border rounded border-gray-200 pt-3 px-4 pb-3 w-full mx-1 relative cursor-pointer hover:border-blue-700 hover:shadow-lg hover:scale-105 hover:duration-200">
+            <div className="text-[12px] text-[#0273ba] pt-5 px-2 pb-1 font-semibold relative">
+              <div className="w-[55px] h-[55px] border-gray-200 border bg-white rounded-full absolute -top-[2.9rem] left-[2.7rem] "></div>
+              Dedicated Sales Engineer
             </div>
-        {/* End of Divs */}
+            <div className="text-[9px] px-1 relative">
+              Helping you buy music hear with confidence.
+              <div className="text-[#0273ba] text-[10px] font-bold mt-6">
+                Learn More
+              </div>
+            </div>
+          </div>
+
+          {/* Fourth Div */}
+          <div className="border rounded border-gray-200 pt-3 px-4 pb-3 w-full mx-1 relative cursor-pointer hover:border-blue-700 hover:shadow-lg hover:scale-105 hover:duration-200">
+            <div className="text-[12px] text-[#0273ba] pt-5 px-2 pb-1 font-semibold relative">
+              <div className="w-[55px] h-[55px] border-gray-200 border bg-white rounded-full absolute -top-[2.9rem] left-[2.7rem] "></div>
+              Dedicated Sales Engineer
+            </div>
+            <div className="text-[9px] px-1 relative">
+              Helping you buy music hear with confidence.
+              <div className="text-[#0273ba] text-[10px] font-bold mt-6">
+                Learn More
+              </div>
+            </div>
+          </div>
+
+          {/* Fifth Div */}
+          <div className="border rounded border-gray-200 pt-3 px-4 pb-3 w-full mx-1 relative cursor-pointer hover:border-blue-700 hover:shadow-lg hover:scale-105 hover:duration-200">
+            <div className="text-[12px] text-[#0273ba] pt-5 px-2 pb-1 font-semibold relative">
+              <div className="w-[55px] h-[55px] border-gray-200 border bg-white rounded-full absolute -top-[2.9rem] left-[2.7rem] "></div>
+              Dedicated Sales Engineer
+            </div>
+            <div className="text-[9px] px-1 relative">
+              Helping you buy music hear with confidence.
+              <div className="text-[#0273ba] text-[10px] font-bold mt-6">
+                Learn More
+              </div>
+            </div>
+          </div>
+
+          {/* Sixth Div */}
+          <div className="border rounded border-gray-200 pt-3 px-4 pb-3 w-full mx-1 relative cursor-pointer hover:border-blue-700 hover:shadow-lg hover:scale-105 hover:duration-200">
+            <div className="text-[12px] text-[#0273ba] pt-5 px-2 pb-1 font-semibold relative">
+              <div className="w-[55px] h-[55px] border-gray-200 border bg-white rounded-full absolute -top-[2.9rem] left-[2.7rem] "></div>
+              Dedicated Sales Engineer
+            </div>
+            <div className="text-[9px] px-1 relative">
+              Helping you buy music hear with confidence.
+              <div className="text-[#0273ba] text-[10px] font-bold mt-6">
+                Learn More
+              </div>
+            </div>
+          </div>
+          {/* End of Divs */}
         </div>
       </div>
     );
