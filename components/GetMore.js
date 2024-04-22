4ad534df455cfd57cd4d99cd248003d9117c22de
Jeffrey Martinez <martinezjf2@gmail.com>
2024-04-22 19:46:45 -0400

    fixed the centering html and css for the get more section at Guitar Store

--- components/GetMore.js
+++ components/GetMore.js
@@ -1,28 +1,35 @@
 import React from "react";
 
 export default function GetMore() {
-    return (
-      <div className="text-center py-2 relative">
-        <div className="text-[30px] font-semibold tracking-wide relative">
-          Get More at GuitarStore
-        </div>
-        <div className="flex  md:w-[80%] m-auto pt-10">
-          {/* First Div */}
-          <div className="border rounded border-gray-200 pt-3 px-4 pb-3 w-full mx-1 relative cursor-pointer hover:border-blue-700 hover:shadow-lg hover:scale-105 hover:duration-200">
-            <div className="text-[12px] text-[#0273ba] pt-5 px-2 pb-1 font-semibold flex justify-center items-center">
-              <div className="w-[55px] h-[55px] border-gray-200 border bg-white rounded-full absolute -top-8 right-[35%]"></div>
+  return (
+    <div className="text-center py-2 relative">
+      <div className="text-[30px] font-semibold tracking-wide relative">
+        Get More at GuitarStore
+      </div>
+      <div className="flex md:w-[80%] m-auto pt-10">
+        {/* First Div */}
+
+        <div className="flex justify-center items-center  w-full">
+          <div className="border rounded pt-5 px-2 pb-1 border-gray-200 mx-1 cursor-pointer hover:border-blue-700 hover:shadow-lg hover:scale-105 hover:duration-200">
+            <div className="relative">
+              <div className="flex justify-center items-center">
+                <div className="w-[55px] h-[55px] border-gray-200 border bg-white rounded-full absolute -top-12 hover:border-blue-700 hover:shadow-lg hover:scale-105 hover:duration-200"></div>
+              </div>
+            </div>
+            <div className="text-[12px] text-[#0273ba] pt-5 px-2 pb-1 font-semibold ">
               Dedicated Sales Engineer
             </div>
             <div className="text-[9px] px-1">
               Helping you buy music hear with confidence.
-              <div className="text-[#0273ba] text-[10px] font-bold mt-6">
-                Learn More
-              </div>
+            </div>
+            <div className="text-[#0273ba] text-[10px] font-bold mt-6">
+              Learn More
             </div>
           </div>
+        </div>
 
-          {/* Second Div Starting Thought */}
-          {/* <div className="border rounded border-gray-200 pt-3 px-4 pb-8 w-full mx-1 relative cursor-pointer hover:border-blue-700 hover:shadow-lg hover:scale-105 hover:duration-200">
+        {/* Second Div Starting Thought */}
+        {/* <div className="border rounded border-gray-200 pt-3 px-4 pb-8 w-full mx-1 relative cursor-pointer hover:border-blue-700 hover:shadow-lg hover:scale-105 hover:duration-200">
             <div className="w-[55px] h-[55px] border-gray-200 border bg-white rounded-full absolute -top-8 right-[3.7rem] hover:border-blue-700 "></div>
             <div className="text-[12px] text-[#0273ba] pt-5 px-2 pb-1 font-semibold">
               55-point Guitar Inpsection
@@ -34,76 +41,106 @@ export default function GetMore() {
               Learn More
             </div>
           </div> */}
-          <div className="border rounded border-gray-200 pt-3 px-4 pb-3 w-full mx-1 relative cursor-pointer hover:border-blue-700 hover:shadow-lg hover:scale-105 hover:duration-200">
-            <div className="text-[12px] text-[#0273ba] pt-5 px-2 pb-1 font-semibold flex justify-center items-center">
-              <div className="w-[55px] h-[55px] border-gray-200 border bg-white rounded-full absolute -top-8 right-[35%]"></div>
+        <div className="flex justify-center items-center  w-full">
+          <div className="border rounded pt-5 px-2 pb-1 border-gray-200 mx-1 cursor-pointer hover:border-blue-700 hover:shadow-lg hover:scale-105 hover:duration-200">
+            <div className="relative">
+              <div className="flex justify-center items-center">
+                <div className="w-[55px] h-[55px] border-gray-200 border bg-white rounded-full absolute -top-12 hover:border-blue-700 hover:shadow-lg hover:scale-105 hover:duration-200"></div>
+              </div>
+            </div>
+            <div className="text-[12px] text-[#0273ba] pt-5 px-2 pb-1 font-semibold ">
               Dedicated Sales Engineer
             </div>
             <div className="text-[9px] px-1">
               Helping you buy music hear with confidence.
-              <div className="text-[#0273ba] text-[10px] font-bold mt-6">
-                Learn More
-              </div>
+            </div>
+            <div className="text-[#0273ba] text-[10px] font-bold mt-6">
+              Learn More
             </div>
           </div>
-
-          {/* Third Div */}
-          <div className="border rounded border-gray-200 pt-3 px-4 pb-3 w-full mx-1 relative cursor-pointer hover:border-blue-700 hover:shadow-lg hover:scale-105 hover:duration-200">
-            <div className="text-[12px] text-[#0273ba] pt-5 px-2 pb-1 font-semibold flex justify-center items-center">
-              <div className="w-[55px] h-[55px] border-gray-200 border bg-white rounded-full absolute -top-8 right-[35%]"></div>
-              Dedicated Sales Engineer
-            </div>
-            <div className="text-[9px] px-1">
-              Helping you buy music hear with confidence.
-              <div className="text-[#0273ba] text-[10px] font-bold mt-6">
-                Learn More
-              </div>
-            </div>
-          </div>
-
-          {/* Fourth Div */}
-          <div className="border rounded border-gray-200 pt-3 px-4 pb-3 w-full mx-1 relative cursor-pointer hover:border-blue-700 hover:shadow-lg hover:scale-105 hover:duration-200">
-            <div className="text-[12px] text-[#0273ba] pt-5 px-2 pb-1 font-semibold flex justify-center items-center">
-              <div className="w-[55px] h-[55px] border-gray-200 border bg-white rounded-full absolute -top-8 right-[35%]"></div>
-              Dedicated Sales Engineer
-            </div>
-            <div className="text-[9px] px-1">
-              Helping you buy music hear with confidence.
-              <div className="text-[#0273ba] text-[10px] font-bold mt-6">
-                Learn More
-              </div>
-            </div>
-          </div>
-
-          {/* Fifth Div */}
-          <div className="border rounded border-gray-200 pt-3 px-4 pb-3 w-full mx-1 relative cursor-pointer hover:border-blue-700 hover:shadow-lg hover:scale-105 hover:duration-200">
-            <div className="text-[12px] text-[#0273ba] pt-5 px-2 pb-1 font-semibold flex justify-center items-center">
-              <div className="w-[55px] h-[55px] border-gray-200 border bg-white rounded-full absolute -top-8 right-[35%]"></div>
-              Dedicated Sales Engineer
-            </div>
-            <div className="text-[9px] px-1">
-              Helping you buy music hear with confidence.
-              <div className="text-[#0273ba] text-[10px] font-bold mt-6">
-                Learn More
-              </div>
-            </div>
-          </div>
-
-          {/* Sixth Div */}
-          <div className="border rounded border-gray-200 pt-3 px-4 pb-3 w-full mx-1 relative cursor-pointer hover:border-blue-700 hover:shadow-lg hover:scale-105 hover:duration-200">
-            <div className="text-[12px] text-[#0273ba] pt-5 px-2 pb-1 font-semibold flex justify-center items-center">
-              <div className="w-[55px] h-[55px] border-gray-200 border bg-white rounded-full absolute -top-8 right-[35%]"></div>
-              Dedicated Sales Engineer
-            </div>
-            <div className="text-[9px] px-1">
-              Helping you buy music hear with confidence.
-              <div className="text-[#0273ba] text-[10px] font-bold mt-6">
-                Learn More
-              </div>
-            </div>
-          </div>
-          {/* End of Divs */}
         </div>
+
+        {/* Third Div */}
+        <div className="flex justify-center items-center  w-full">
+          <div className="border rounded pt-5 px-2 pb-1 border-gray-200 mx-1 cursor-pointer hover:border-blue-700 hover:shadow-lg hover:scale-105 hover:duration-200">
+            <div className="relative">
+              <div className="flex justify-center items-center">
+                <div className="w-[55px] h-[55px] border-gray-200 border bg-white rounded-full absolute -top-12 hover:border-blue-700 hover:shadow-lg hover:scale-105 hover:duration-200"></div>
+              </div>
+            </div>
+            <div className="text-[12px] text-[#0273ba] pt-5 px-2 pb-1 font-semibold ">
+              Dedicated Sales Engineer
+            </div>
+            <div className="text-[9px] px-1">
+              Helping you buy music hear with confidence.
+            </div>
+            <div className="text-[#0273ba] text-[10px] font-bold mt-6">
+              Learn More
+            </div>
+          </div>
+        </div>
+
+        {/* Fourth Div */}
+        <div className="flex justify-center items-center  w-full">
+          <div className="border rounded pt-5 px-2 pb-1 border-gray-200 mx-1 cursor-pointer hover:border-blue-700 hover:shadow-lg hover:scale-105 hover:duration-200">
+            <div className="relative">
+              <div className="flex justify-center items-center">
+                <div className="w-[55px] h-[55px] border-gray-200 border bg-white rounded-full absolute -top-12 hover:border-blue-700 hover:shadow-lg hover:scale-105 hover:duration-200"></div>
+              </div>
+            </div>
+            <div className="text-[12px] text-[#0273ba] pt-5 px-2 pb-1 font-semibold ">
+              Dedicated Sales Engineer
+            </div>
+            <div className="text-[9px] px-1">
+              Helping you buy music hear with confidence.
+            </div>
+            <div className="text-[#0273ba] text-[10px] font-bold mt-6">
+              Learn More
+            </div>
+          </div>
+        </div>
+
+        {/* Fifth Div */}
+        <div className="flex justify-center items-center  w-full">
+          <div className="border rounded pt-5 px-2 pb-1 border-gray-200 mx-1 cursor-pointer hover:border-blue-700 hover:shadow-lg hover:scale-105 hover:duration-200">
+            <div className="relative">
+              <div className="flex justify-center items-center">
+                <div className="w-[55px] h-[55px] border-gray-200 border bg-white rounded-full absolute -top-12 hover:border-blue-700 hover:shadow-lg hover:scale-105 hover:duration-200"></div>
+              </div>
+            </div>
+            <div className="text-[12px] text-[#0273ba] pt-5 px-2 pb-1 font-semibold ">
+              Dedicated Sales Engineer
+            </div>
+            <div className="text-[9px] px-1">
+              Helping you buy music hear with confidence.
+            </div>
+            <div className="text-[#0273ba] text-[10px] font-bold mt-6">
+              Learn More
+            </div>
+          </div>
+        </div>
+
+        {/* Sixth Div */}
+        <div className="flex justify-center items-center  w-full">
+          <div className="border rounded pt-5 px-2 pb-1 border-gray-200 mx-1 cursor-pointer hover:border-blue-700 hover:shadow-lg hover:scale-105 hover:duration-200">
+            <div className="relative">
+              <div className="flex justify-center items-center">
+                <div className="w-[55px] h-[55px] border-gray-200 border bg-white rounded-full absolute -top-12 hover:border-blue-700 hover:shadow-lg hover:scale-105 hover:duration-200"></div>
+              </div>
+            </div>
+            <div className="text-[12px] text-[#0273ba] pt-5 px-2 pb-1 font-semibold ">
+              Dedicated Sales Engineer
+            </div>
+            <div className="text-[9px] px-1">
+              Helping you buy music hear with confidence.
+            </div>
+            <div className="text-[#0273ba] text-[10px] font-bold mt-6">
+              Learn More
+            </div>
+          </div>
+        </div>
+        {/* End of Divs */}
       </div>
-    );
+    </div>
+  );
 }
