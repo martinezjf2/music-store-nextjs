04f3a4df8968928e1b84d348eebf81dd8e1027e8
Jeffrey Martinez <martinezjf2@gmail.com>
2024-04-28 20:26:52 -0400

    added gradient on the header section

--- components/Header.js
+++ components/Header.js
@@ -5,10 +5,12 @@ export default function Header() {
     <div className="flex w-[80%] mx-auto my-5">
       <div className="relative bg-black text-white h-[18rem] w-8/12 py-11 px-9 overflow-hidden">
         <div className="absolute inset-0 bg-gradient-to-l from-transparent to-black z-15"></div>
-        <a href="/"><img
-          src="https://www.hollywoodreporter.com/wp-content/uploads/2017/08/gettyimages-687020126_-_h_2017_0.jpg?w=1296"
-          className="absolute w-full -top-6 left-0"
-        /></a>
+        <a href="/">
+          <img
+            src="https://www.hollywoodreporter.com/wp-content/uploads/2017/08/gettyimages-687020126_-_h_2017_0.jpg?w=1296"
+            className="absolute w-full -top-6 left-0"
+          />
+        </a>
         <div className="z-20 relative">
           <div className="font-bold text-[48px] mb-1">
             Electric Guitar Month
@@ -30,7 +32,9 @@ export default function Header() {
           <div className="text-[70px] font-extrabold leading-none">
             0<span className="text-[30px]">%</span>
           </div>
-          <div className="font-extrabold text-[44px] leading-none">INTEREST</div>
+          <div className="font-extrabold text-[44px] leading-none">
+            INTEREST
+          </div>
           <div className="font-extrabold text-[24px] leading-none">
             FOR 48 MONTHS
           </div>
@@ -39,7 +43,7 @@ export default function Header() {
             Now-April 30.
           </div>
         </div>
-        <div className="bg-[#0273ba] h-[5.4rem] leading-none">
+        <div className="bg-[#177fc0] bg-gradient-to-r from-[#4fa8df] to-[#0068a8] h-[5.4rem] leading-none">
           <div className="">
             <button className="mt-6 bg-white py-2 px-8 text-[13px] rounded font-semibold">
               Shop Now
