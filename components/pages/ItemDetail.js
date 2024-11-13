3b7c8cddebce5f002a412eb43b155fcaacebac32
Jeffrey Martinez <martinezjf2@gmail.com>
2024-11-13 14:23:59 -0500

    changed the titles on pages from checkout and from itemDetail component

--- components/pages/ItemDetail.js
+++ components/pages/ItemDetail.js
@@ -4,6 +4,7 @@ import { faHeart } from "@fortawesome/free-regular-svg-icons";
 import { useCart } from "@/context/CartContext"; // Ensure this import is correct
 import Image from "next/image";
 import Link from "next/link";
+import Head from "next/head";
 
 export default function ItemDetail({ item, storeName }) {
   const { addItemToCart, removeItemFromCart } = useCart(); // Accessing the Cart State
@@ -41,12 +42,16 @@ export default function ItemDetail({ item, storeName }) {
   }
 
   return (
-    <section className="mx-auto relative px-10 py-6 ">
-      <div className="flex justify-start items-center">
-        {/* <a href="/">Home</a> */}
-        <Link href="/">Home</Link>
-        <div className="w-[5px] h-[5px] bg-black rounded-full flex justify-center items-center mx-3"></div>
-        {/* <a
+    <>
+      <Head>
+        <title>{name}</title>
+      </Head>
+      <section className="mx-auto relative px-10 py-6 ">
+        <div className="flex justify-start items-center">
+          {/* <a href="/">Home</a> */}
+          <Link href="/">Home</Link>
+          <div className="w-[5px] h-[5px] bg-black rounded-full flex justify-center items-center mx-3"></div>
+          {/* <a
           href={`/${storeName
             .toLowerCase()
             .replace(/&/g, "and")
@@ -54,129 +59,132 @@ export default function ItemDetail({ item, storeName }) {
         >
           {storeName}
         </a> */}
-        <Link
-          href={`/${storeName
-            .toLowerCase()
-            .replace(/&/g, "and")
-            .replace(/\s+/g, "-")}`}
-        >
-          {storeName}
-        </Link>
+          <Link
+            href={`/${storeName
+              .toLowerCase()
+              .replace(/&/g, "and")
+              .replace(/\s+/g, "-")}`}
+          >
+            {storeName}
+          </Link>
 
-        <div className="w-[5px] h-[5px] bg-black rounded-full flex justify-center items-center mx-3"></div>
-        <span className="font-bold">
-          {name.length > 50 ? name.substring(0, 45) + "..." : name}
-        </span>
-      </div>
-
-      <div className="mt-7 flex justify-around pt-16 pb-10 border-t-2 w-11/12 mx-auto border-b-2">
-        {/* Image Section */}
-        <div className="border-r-2 w-8/12">
-          {/* <img src={image} className="w-[800px]" alt={name} /> */}
-          <Image
-            src={image}
-            width={200}
-            height={200}
-            alt={image}
-            className="w-[800px] pr-11"
-          />
+          <div className="w-[5px] h-[5px] bg-black rounded-full flex justify-center items-center mx-3"></div>
+          <span className="font-bold">
+            {name.length > 50 ? name.substring(0, 45) + "..." : name}
+          </span>
         </div>
 
-        {/* Content Section */}
-        <div className="w-4/12 text-left pl-9 mt-2">
-          {special && (
-            <div
-              className={`border py-1 w-[100px] flex justify-center items-center text-[13px] font-bold tracking-wide text-white ${classForBackgroundColor()}`}
-            >
-              {special}
-            </div>
-          )}
+        <div className="mt-7 flex justify-around pt-16 pb-10 border-t-2 w-11/12 mx-auto border-b-2">
+          {/* Image Section */}
+          <div className="border-r-2 w-8/12">
+            {/* <img src={image} className="w-[800px]" alt={name} /> */}
+            <Image
+              src={image}
+              width={200}
+              height={200}
+              alt={image}
+              className="w-[800px] pr-11"
+            />
+          </div>
 
-          <div className="pt-4 font-extrabold tracking-wide text-[32px]">
-            {name}
-          </div>
-          <div className="mt-3 mb-2">
-            <span className="font-bold">Condition:</span> New
-          </div>
-          <div className="font-bold text-[24px] pb-5">${price}</div>
-          <hr className="font-extrabold" />
-          <div className="mx-7 my-4 text-sm flex justify-center items-center font-bold">
-            6-month special financing + $11 back in Rewards
-            <br />
-            Limited time
-          </div>
-          <hr className="font-extrabold" />
-          <div className="my-3 mx-2 text-sm">
-            Pay in 4 interest-free payments of ${parseFloat(price) / 4.0}.
-          </div>
-          <hr className="font-extrabold" />
-          <div className="my-3 mx-2 text-sm">Lease-To-Own Purchase Options</div>
-          <hr className="font-extrabold" />
-          <div className="my-9 bg-gray-50 p-6">
-            <div>
-              <div className="text-red-500 font-medium tracking-widest text-md mb-4">
-                <i className="fa-solid fa-store text-black mr-2 text-[22px]"></i>
-                Check store availability
+          {/* Content Section */}
+          <div className="w-4/12 text-left pl-9 mt-2">
+            {special && (
+              <div
+                className={`border py-1 w-[100px] flex justify-center items-center text-[13px] font-bold tracking-wide text-white ${classForBackgroundColor()}`}
+              >
+                {special}
               </div>
-              <div className="tracking-widest text-md mb-1">
-                <i className="fa-solid fa-truck-fast text-black mr-3 text-[22px]"></i>
-                <span className="font-bold">Free Delivery</span> to{" "}
-                <span className="font-bold underline">United States</span>
-              </div>
-              <span className="text-green-800 tracking-wider text-md block ml-10">
-                In-stock
-              </span>
-            </div>
-          </div>
-          <div className="flex mb-7">
-            <div
-              className={`w-10/12 flex justify-center items-center tracking-wider text-white font-bold p-3 rounded shadow-lg hover:cursor-pointer ${
-                isActive ? "bg-green-600" : "bg-red-600"
-              }`}
-              onClick={handleActivateButton}
-            >
-              {isActive ? "Item Added" : "Add to Cart"}
-            </div>
-            <div className="w-2/12 flex justify-center items-center">
-              <FontAwesomeIcon
-                icon={faHeart}
-                className="hover:cursor-pointer text-[25px] text-gray-500"
-              />
-            </div>
-          </div>
-          <hr className="font-extrabold" />
-          <div className="flex justify-evenly items-center my-5 py-1 text-center">
-            <div className="text-sm font-light px-1">
-              <i className="fa-solid fa-truck hover:cursor-pointer text-[25px] text-gray-800"></i>
-              <br />
-              <div className="text-sm font-light mt-2">Free Delivery</div>
-            </div>
-            <div className="text-sm font-light px-1 ">
-              <i className="fa-solid fa-reply hover:cursor-pointer text-[25px] text-gray-800"></i>
-              <br />
-              <div className="text-sm font-light mt-2">45 Day Return</div>
-            </div>
-            <div className="text-sm font-light px-1">
-              <i className="fa-solid fa-headset hover:cursor-pointer text-[25px] text-gray-800"></i>
-              <br />
-              <div className="text-sm font-light mt-2">Online Support</div>
-            </div>
-            <div className="text-sm font-light px-1">
-              <i className="fa-solid fa-circle-check hover:cursor-pointer text-[25px] text-gray-800"></i>
-              <br />
-              <div className="text-sm font-light mt-2">2-Year Warranty</div>
-            </div>
-          </div>
-          <hr className="font-extrabold" />
-        </div>
-      </div>
+            )}
 
-      <div className="py-7 px-9">
-        <div className="font-bold text-[26px] mb-5 text-center">
-          Description
+            <div className="pt-4 font-extrabold tracking-wide text-[32px]">
+              {name}
+            </div>
+            <div className="mt-3 mb-2">
+              <span className="font-bold">Condition:</span> New
+            </div>
+            <div className="font-bold text-[24px] pb-5">${price}</div>
+            <hr className="font-extrabold" />
+            <div className="mx-7 my-4 text-sm flex justify-center items-center font-bold">
+              6-month special financing + $11 back in Rewards
+              <br />
+              Limited time
+            </div>
+            <hr className="font-extrabold" />
+            <div className="my-3 mx-2 text-sm">
+              Pay in 4 interest-free payments of ${parseFloat(price) / 4.0}.
+            </div>
+            <hr className="font-extrabold" />
+            <div className="my-3 mx-2 text-sm">
+              Lease-To-Own Purchase Options
+            </div>
+            <hr className="font-extrabold" />
+            <div className="my-9 bg-gray-50 p-6">
+              <div>
+                <div className="text-red-500 font-medium tracking-widest text-md mb-4">
+                  <i className="fa-solid fa-store text-black mr-2 text-[22px]"></i>
+                  Check store availability
+                </div>
+                <div className="tracking-widest text-md mb-1">
+                  <i className="fa-solid fa-truck-fast text-black mr-3 text-[22px]"></i>
+                  <span className="font-bold">Free Delivery</span> to{" "}
+                  <span className="font-bold underline">United States</span>
+                </div>
+                <span className="text-green-800 tracking-wider text-md block ml-10">
+                  In-stock
+                </span>
+              </div>
+            </div>
+            <div className="flex mb-7">
+              <div
+                className={`w-10/12 flex justify-center items-center tracking-wider text-white font-bold p-3 rounded shadow-lg hover:cursor-pointer ${
+                  isActive ? "bg-green-600" : "bg-red-600"
+                }`}
+                onClick={handleActivateButton}
+              >
+                {isActive ? "Item Added" : "Add to Cart"}
+              </div>
+              <div className="w-2/12 flex justify-center items-center">
+                <FontAwesomeIcon
+                  icon={faHeart}
+                  className="hover:cursor-pointer text-[25px] text-gray-500"
+                />
+              </div>
+            </div>
+            <hr className="font-extrabold" />
+            <div className="flex justify-evenly items-center my-5 py-1 text-center">
+              <div className="text-sm font-light px-1">
+                <i className="fa-solid fa-truck hover:cursor-pointer text-[25px] text-gray-800"></i>
+                <br />
+                <div className="text-sm font-light mt-2">Free Delivery</div>
+              </div>
+              <div className="text-sm font-light px-1 ">
+                <i className="fa-solid fa-reply hover:cursor-pointer text-[25px] text-gray-800"></i>
+                <br />
+                <div className="text-sm font-light mt-2">45 Day Return</div>
+              </div>
+              <div className="text-sm font-light px-1">
+                <i className="fa-solid fa-headset hover:cursor-pointer text-[25px] text-gray-800"></i>
+                <br />
+                <div className="text-sm font-light mt-2">Online Support</div>
+              </div>
+              <div className="text-sm font-light px-1">
+                <i className="fa-solid fa-circle-check hover:cursor-pointer text-[25px] text-gray-800"></i>
+                <br />
+                <div className="text-sm font-light mt-2">2-Year Warranty</div>
+              </div>
+            </div>
+            <hr className="font-extrabold" />
+          </div>
         </div>
-        <div className="font-extralight leading-7">{description}</div>
-      </div>
-    </section>
+
+        <div className="py-7 px-9">
+          <div className="font-bold text-[26px] mb-5 text-center">
+            Description
+          </div>
+          <div className="font-extralight leading-7">{description}</div>
+        </div>
+      </section>
+    </>
   );
 }
--- pages/checkout.js
+++ pages/checkout.js
@@ -1,6 +1,7 @@
 import { useContext, useEffect, useState } from "react";
 import { useCart } from "../context/CartContext";
 import Image from "next/image";
+import Head from "next/head";
 import { loadStripe } from "@stripe/stripe-js";
 
 const stripePromise = loadStripe(
@@ -99,64 +100,69 @@ export default function CheckoutPage() {
   ));
 
   return (
-    <section className="w-8/12 mx-auto shadow-2xl rounded-bl-md mb-24 rounded-br-md relative pb-5">
-      <div className="mt-6 flex">
-        <div className="bg-[#015c96] text-white py-[7px] flex justify-center items-center w-11 rounded-tl-md">
-          <i className="fa-solid fa-cart-shopping text-[22px]"></i>
-        </div>
-        <div className="bg-[#0072bb] text-white text-[20px] font-semibold tracking-wider px-2 flex justify-left items-center w-full rounded-tr-md">
-          Shopping Cart
-        </div>
-      </div>
-      <div className="bg-[#edf1f9] flex justify-between items-center py-2">
-        <div className="ml-2 text-[9px]">PRODUCT</div>
-        <div className="flex justify-evenly items-center">
-          <div className="mr-12 text-[9px]">QTY.</div>
-          <div className="mr-3 text-[9px]">PRICE</div>
-        </div>
-      </div>
-
-      {cart.length === 0 ? (
-        <h1 className="text-center mt-10 text-lg font-bold pb-28">
-          There is nothing in the cart!
-        </h1>
-      ) : (
-        allItems
-      )}
-
-      {cart.length > 0 && (
-        <div>
-          <div className="flex justify-end text-right mt-3 mb-4">
-            <div className="mr-6 mt-1">
-              <div className="font-bold text-[10px]">SUBTOTAL</div>
-              <div className="font-bold text-[10px] mt-2">
-                ESTIMATED SHIPPING
-              </div>
-              <div className="font-light text-[8px] mt-1">
-                Additional shipping methods will be available during checkout!
-              </div>
-            </div>
-            <div className="mr-4">
-              <div className="font-bold text-[14px]">
-                ${calculateTotal().toFixed(2)}
-              </div>
-              <div className="text-red-600 font-extrabold text-[14px]">
-                FREE
-              </div>
-            </div>
+    <>
+      <Head>
+        <title>Checkout - GuitarStore</title>
+      </Head>
+      <section className="w-8/12 mx-auto shadow-2xl rounded-bl-md mb-24 rounded-br-md relative pb-5">
+        <div className="mt-6 flex">
+          <div className="bg-[#015c96] text-white py-[7px] flex justify-center items-center w-11 rounded-tl-md">
+            <i className="fa-solid fa-cart-shopping text-[22px]"></i>
           </div>
-          <div className="h-[1px] bg-gray-200 w-7/12 absolute right-0"></div>
-          <div className="flex justify-end mr-3 pb-4 pt-6">
-            <button
-              onClick={handleCheckout}
-              disabled={loading}
-              className="bg-[#3b7d1b] text-white font-semibold py-2 px-20 rounded hover:cursor-pointer"
-            >
-              {loading ? "Processing..." : "CHECKOUT"}
-            </button>
+          <div className="bg-[#0072bb] text-white text-[20px] font-semibold tracking-wider px-2 flex justify-left items-center w-full rounded-tr-md">
+            Shopping Cart
           </div>
         </div>
-      )}
-    </section>
+        <div className="bg-[#edf1f9] flex justify-between items-center py-2">
+          <div className="ml-2 text-[9px]">PRODUCT</div>
+          <div className="flex justify-evenly items-center">
+            <div className="mr-12 text-[9px]">QTY.</div>
+            <div className="mr-3 text-[9px]">PRICE</div>
+          </div>
+        </div>
+
+        {cart.length === 0 ? (
+          <h1 className="text-center mt-10 text-lg font-bold pb-28">
+            There is nothing in the cart!
+          </h1>
+        ) : (
+          allItems
+        )}
+
+        {cart.length > 0 && (
+          <div>
+            <div className="flex justify-end text-right mt-3 mb-4">
+              <div className="mr-6 mt-1">
+                <div className="font-bold text-[10px]">SUBTOTAL</div>
+                <div className="font-bold text-[10px] mt-2">
+                  ESTIMATED SHIPPING
+                </div>
+                <div className="font-light text-[8px] mt-1">
+                  Additional shipping methods will be available during checkout!
+                </div>
+              </div>
+              <div className="mr-4">
+                <div className="font-bold text-[14px]">
+                  ${calculateTotal().toFixed(2)}
+                </div>
+                <div className="text-red-600 font-extrabold text-[14px]">
+                  FREE
+                </div>
+              </div>
+            </div>
+            <div className="h-[1px] bg-gray-200 w-7/12 absolute right-0"></div>
+            <div className="flex justify-end mr-3 pb-4 pt-6">
+              <button
+                onClick={handleCheckout}
+                disabled={loading}
+                className="bg-[#3b7d1b] text-white font-semibold py-2 px-20 rounded hover:cursor-pointer"
+              >
+                {loading ? "Processing..." : "CHECKOUT"}
+              </button>
+            </div>
+          </div>
+        )}
+      </section>
+    </>
   );
 }
