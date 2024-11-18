184874e9de93d1c09210345f293b203ba67e5429
Jeffrey Martinez <martinezjf2@gmail.com>
2024-11-17 20:26:49 -0500

    added responsive design on the checkout page

--- pages/checkout.js
+++ pages/checkout.js
@@ -1,4 +1,4 @@
-import { useContext, useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import { useCart } from "../context/CartContext";
 import Image from "next/image";
 import Head from "next/head";
@@ -10,21 +10,19 @@ const stripePromise = loadStripe(
 
 export default function CheckoutPage() {
   const { cart, removeItemFromCart, calculateTotal } = useCart();
-  const [isClient, setIsClient] = useState(false); // Track if it's client-side
+  const [isClient, setIsClient] = useState(false);
   const [loading, setLoading] = useState(false);
 
   useEffect(() => {
-    setIsClient(true); // Set to true when the component mounts
+    setIsClient(true);
   }, []);
 
-  // Optionally handle loading or provide a fallback if necessary
   if (!isClient) {
-    return <div>Loading...</div>; // Wait for client-side rendering
+    return <div>Loading...</div>;
   }
 
   const handleCheckout = async () => {
     setLoading(true);
-    console.log({ cart });
 
     try {
       const stripe = await stripePromise;
@@ -39,14 +37,13 @@ export default function CheckoutPage() {
             name: item.name,
             image: item.image,
             price: item.price,
-            quantity: 1, // Adjust this if quantity is dynamic
+            quantity: 1,
           })),
         }),
       });
 
       const { sessionId } = await response.json();
 
-      // Redirect to Stripe Checkout
       await stripe.redirectToCheckout({ sessionId });
     } catch (error) {
       console.error("Error in checkout:", error);
@@ -57,45 +54,50 @@ export default function CheckoutPage() {
   };
 
   const allItems = cart.map((item, index) => (
-    <div key={index}>
-      <div className="flex mb-5 mt-4 mx-1">
-        <div className="w-2/12 ml-2 mr-6 flex justify-center items-center">
-          <Image src={item.image} alt={item.name} width={150} height={150} />
+    <div
+      key={index}
+      className="flex flex-wrap md:flex-nowrap items-center mb-5 px-3 py-4"
+    >
+      <div className="w-full md:w-2/12 flex justify-center items-center mb-3 md:mb-0">
+        <Image
+          src={item.image}
+          alt={item.name}
+          width={120}
+          height={120}
+          className="w-[120px] h-auto object-cover"
+        />
+      </div>
+      <div className="w-full md:w-6/12 px-2">
+        <div className="font-bold text-[14px] md:text-[16px] text-[#0a3b6b] hover:underline hover:cursor-pointer">
+          {item.name}
         </div>
-        <div className="w-8/12">
-          <div className="font-bold text-[16px] text-[#0a3b6b] hover:underline hover:cursor-pointer">
-            {item.name}
-          </div>
-          <div className="text-[9px] mt-1 font-light">
-            {item.description.length > 150
-              ? item.description.substring(0, 135) + "..."
-              : item.description}
-          </div>
-          <div className="text-[10px] mt-1 flex justify-start">
-            <div>Condition: {item.condition}</div>
-            <div className="ml-4">Price: ${item.price}</div>
-          </div>
-          <div className="text-red-600 text-[10px] mt-1">
-            <span className="font-bold">Promo: 36 Month</span> Promotional
-            Financing Available.
-          </div>
+        <div className="text-[9px] mt-1 font-light">
+          {item.description.length > 150
+            ? item.description.substring(0, 135) + "..."
+            : item.description}
         </div>
-        <div className="flex justify-end w-2/12">
-          <div className="w-6/12 text-right font-bold text-[14px] mr-4">
-            1 <br />
-            <span
-              className="text-[#0a3b6b] hover:underline hover:cursor-pointer text-[10px] font-light"
-              onClick={() => removeItemFromCart(item)}
-            >
-              Remove
-            </span>
-          </div>
-          <div className="w-6/12 text-right font-bold text-[14px] mr-2">
-            ${item.price}
-          </div>
+        <div className="text-[10px] mt-1 flex justify-start">
+          <div>Condition: {item.condition}</div>
+          <div className="ml-4">Price: ${item.price}</div>
+        </div>
+        <div className="text-red-600 text-[10px] mt-1">
+          <span className="font-bold">Promo: 36 Month</span> Promotional
+          Financing Available.
         </div>
       </div>
-      <hr />
+      <div className="w-full md:w-4/12 flex justify-between items-center">
+        <div className="text-center font-bold text-[14px]">
+          1
+          <br />
+          <span
+            className="text-[#0a3b6b] hover:underline hover:cursor-pointer text-[10px] font-light"
+            onClick={() => removeItemFromCart(item)}
+          >
+            Remove
+          </span>
+        </div>
+        <div className="font-bold text-[14px]">${item.price}</div>
+      </div>
     </div>
   ));
 
@@ -104,23 +106,28 @@ export default function CheckoutPage() {
       <Head>
         <title>Checkout - GuitarStore</title>
       </Head>
-      <section className="w-8/12 mx-auto shadow-2xl rounded-bl-md mb-24 rounded-br-md relative pb-5">
+      <section className="w-full max-w-6xl mx-auto shadow-2xl rounded-md mb-24 relative pb-5  sm:max-w-xl md:max-w-3xl lg:max-w-4xl">
+        {/* Header */}
         <div className="mt-6 flex">
           <div className="bg-[#015c96] text-white py-[7px] flex justify-center items-center w-11 rounded-tl-md">
             <i className="fa-solid fa-cart-shopping text-[22px]"></i>
           </div>
-          <div className="bg-[#0072bb] text-white text-[20px] font-semibold tracking-wider px-2 flex justify-left items-center w-full rounded-tr-md">
+          <div className="bg-[#0072bb] text-white text-[16px] md:text-[20px] font-semibold tracking-wider px-2 flex items-center w-full rounded-tr-md">
             Shopping Cart
           </div>
         </div>
-        <div className="bg-[#edf1f9] flex justify-between items-center py-2">
-          <div className="ml-2 text-[9px]">PRODUCT</div>
-          <div className="flex justify-evenly items-center">
-            <div className="mr-12 text-[9px]">QTY.</div>
-            <div className="mr-3 text-[9px]">PRICE</div>
+
+        {/* Table Header */}
+        <div className="hidden md:flex bg-[#edf1f9] justify-between items-center py-2 px-3">
+          <div className="w-2/12 text-[9px] text-center">PRODUCT</div>
+          <div className="w-6/12 text-[9px] text-left">DETAILS</div>
+          <div className="w-4/12 text-[9px] text-center flex justify-between">
+            <span>QTY.</span>
+            <span>PRICE</span>
           </div>
         </div>
 
+        {/* Cart Items */}
         {cart.length === 0 ? (
           <h1 className="text-center mt-10 text-lg font-bold pb-28">
             There is nothing in the cart!
@@ -129,9 +136,10 @@ export default function CheckoutPage() {
           allItems
         )}
 
+        {/* Checkout Section */}
         {cart.length > 0 && (
           <div>
-            <div className="flex justify-end text-right mt-3 mb-4">
+            <div className="flex flex-wrap justify-end text-right mt-3 mb-4">
               <div className="mr-6 mt-1">
                 <div className="font-bold text-[10px]">SUBTOTAL</div>
                 <div className="font-bold text-[10px] mt-2">
@@ -150,12 +158,12 @@ export default function CheckoutPage() {
                 </div>
               </div>
             </div>
-            <div className="h-[1px] bg-gray-200 w-7/12 absolute right-0"></div>
-            <div className="flex justify-end mr-3 pb-4 pt-6">
+            <div className="h-[1px] bg-gray-200 w-full md:w-7/12 mx-auto"></div>
+            <div className="flex justify-center md:justify-end mr-3 pb-4 pt-6">
               <button
                 onClick={handleCheckout}
                 disabled={loading}
-                className="bg-[#3b7d1b] text-white font-semibold py-2 px-20 rounded hover:cursor-pointer"
+                className="bg-[#3b7d1b] text-white font-semibold py-2 px-10 md:px-20 rounded hover:cursor-pointer"
               >
                 {loading ? "Processing..." : "CHECKOUT"}
               </button>
